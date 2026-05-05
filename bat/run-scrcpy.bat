67c7fd9b6b670d441586061abec326a7dae01ded
CrazyJ36 <crazy_j36@hotmail.com>
2026-05-05 04:53:38 -0400

    u

--- bat/run-scrcpy.bat
+++ bat/run-scrcpy.bat
@@ -1,2 +1,3 @@
 cd "%PROGRAMFILES%\scrcpy-win64-v3.3.4"
-start "" "%PROGRAMFILES%\Git\git-bash.exe" -c "./scrcpy.exe -s 56271FDCQ0040Q; exec bash"
+start "" "%PROGRAMFILES%\Git\git-bash.exe" -c "./scrcpy.exe -s 56271FDCQ0040Q --new-display=900x480/120; exec bash"
+REM --start-app=com.crazyj36.wearnews
